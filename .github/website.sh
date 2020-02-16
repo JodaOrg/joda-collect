4694c0dec231dd2b8d2a96560dbb8862be61461b
Stephen Colebourne (CI) <scolebourne@joda.org>
2020-02-16 12:31:01 +0000

    Add GitHub actions, remove Travis

--- .github/website.sh
+++ .github/website.sh
@@ -16,7 +16,7 @@ cp -R ../site joda-collect/
 echo "## update..."
 git add -A
 git status
-git commit --message "Update joda-collect from Travis: Build $TRAVIS_BUILD_NUMBER"
+git commit --message "Update joda-beans from CI: $GITHUB_ACTION"
 
 echo "## push..."
 git push origin master
