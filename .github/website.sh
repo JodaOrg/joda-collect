4efcc95b81ab9ab37c6ce47b4b33eed27042c15f
Stephen Colebourne <scolebourne@joda.org>
2022-08-24 18:02:51 +0100

    Switch to main, Update build scripts, dependabot (#5)

--- .github/website.sh
+++ .github/website.sh
@@ -16,9 +16,9 @@ cp -R ../site joda-collect/
 echo "## update..."
 git add -A
 git status
-git commit --message "Update joda-beans from CI: $GITHUB_ACTION"
+git commit --message "Update joda-collect from CI: $GITHUB_ACTION"
 
 echo "## push..."
-git push origin master
+git push origin main
 
 echo "## done"
